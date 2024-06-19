aa78b88010764ce043d6762a13eda6db88aea4d0
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-06-18 21:53:30 -0300

    query params

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ const express = require("express");
 const app = express();
 app.get("/message/:id/:user", (req, res) => {
   const { id, user } = req.params
+
   res.send(`ID da mensagem: ${id}. Para o usuário: ${user}`);
 });
+
+app.get("/users", (req, res) => {
+  const { page, limit} = req.query
+
+  res.send(`Página: ${page}. Mostrar: ${limit}`)
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
