defbd9bef31fd6833c95186ed574f0b83082170f
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-06-18 23:21:33 -0300

    how to use post method

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 
 const app = express();
-app.get("/message/:id/:user", (req, res) => {
-  const { id, user } = req.params
+app.use(express.json());
+// app.get("/message/:id/:user", (req, res) => {
+//   const { id, user } = req.params
 
-  res.send(`ID da mensagem: ${id}. Para o usuário: ${user}`);
+//   res.send(`ID da mensagem: ${id}. Para o usuário: ${user}`);
+// });
+
+app.post("/users", (req, res) => {
+  // const { page, limit} = req.query
+  const { name, email, password } = req.body;
+
+  res.json({ name, email, password });
 });
-
-app.get("/users", (req, res) => {
-  const { page, limit} = req.query
-
-  res.send(`Página: ${page}. Mostrar: ${limit}`)
-})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
