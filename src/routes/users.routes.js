4bacb44e58e11422f153113d1a2d4dcb61244a31
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-06-28 20:37:28 -0300

    update name and email

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -10,5 +10,6 @@ const usersRoutes = Router()
 const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
