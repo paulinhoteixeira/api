21616c73ac4749566716b5bd171fc781d256ba17
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2024-07-12 21:25:04 -0300

    index notes function

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -54,6 +54,30 @@ class NotesController{
 
     return response.json()
   }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query
+
+    let notes;
+
+    if(tags){
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("tags")
+      .whereIn("name", filterTags)
+
+    }else{
+      notes = await knex("notes")
+      .where({ user_id})
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+    }
+
+    
+
+    return response.json(notes)
+
+  }
 }
 
 module.exports = NotesController;
